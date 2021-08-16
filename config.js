8081d58cd571d53bec40908de1f8a15385a2a7c2
mrshinx <45674057+mrshinx@users.noreply.github.com>
2021-08-16 22:30:58 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+
+// edit your experiment here: 
+// define the sequence of images and their duration
+// 1. column = image file
+// 2. column = duration in seconds
+// important: all images should have the same size. the size of the first image defines the size of the eyetracking area. 
+
+var experiment = 
+[
+['experiment_1024x768px.jpg', 5],
+['frog_1024x768px.jpg', 5]
+];
+
+
+
+// URLs fpr the machine learning models
+
+//const FACEMESH_GRAPHMODEL_URL = 'https://tfhub.dev/mediapipe/tfjs-model/facemesh/1/default/1';
+const FACEMESH_GRAPHMODEL_URL = 'https://mediablix.de/facemesh_1';
+const BLAZEFACE_MODEL_URL="https://mediablix.de/blazeface_1";
+
